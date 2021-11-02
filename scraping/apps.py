9dcd40c4531150fcbbd57abcc8de23680bb432e5
lavrem <lavrem@mail.ru>
2021-11-02 11:24:09 +0300

    init_commit

---
+++ scraping/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ScrapingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'scraping'
+    verbose_name='Приложение сбора вакансий'
+    verbose_name_plural = 'Приложения сбора вакансий'
